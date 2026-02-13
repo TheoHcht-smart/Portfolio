cb2026189cbd063b7c01eb9b106749a43cb52a9f
TheoHcht-smart <theo9925.huchot@gmail.com>
2026-02-13 18:19:58 +0100

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import { ThemeToggle } from "../components/ThemeToggle";
+
+export const Home = () => {
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      {/* Mode thème */}
+    <ThemeToggle />
+      {/* Effets du BG */}
+
+      {/* Navbar*/}
+
+      {/* contenu principal */}
+
+      {/* Footer */}
+    </div>
+  );
+}
