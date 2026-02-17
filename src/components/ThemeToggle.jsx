8bca797ee8099f96596fc37b0c97461eb99a38a5
TheoHcht-smart <theo9925.huchot@gmail.com>
2026-02-17 13:37:55 +0100

    Stars BG

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,5 +1,6 @@
 import { Sun,Moon } from 'lucide-react';
 import{useEffect, useState} from 'react';
+import { cn } from '../lib/utils';
 
 export const ThemeToggle = () =>{
 
@@ -30,7 +31,12 @@ export const ThemeToggle = () =>{
     }}
 
     return (
-    <button onClick={toggleTheme}>{isDarkMode ? <Sun className='h-6 w-6 text-yellow-300'/> : <Moon className='h-6 w-6 text-blue-900'/>}</button>
+    <button onClick={toggleTheme} className={cn("fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300"
+        ,"focus:outline-hidden")}>
+        {isDarkMode ? 
+        (<Sun className='h-6 w-6 text-yellow-300'/>) : 
+        (<Moon className='h-6 w-6 text-blue-900'/>)
+    }</button>
 );
 
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import { ThemeToggle } from "../components/ThemeToggle";
+import { StarBackground } from "../components/StarBackGround.jsx";
+
 
 export const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
       {/* Mode thème */}
     <ThemeToggle />
-      {/* Effets du BG */}
-
+      {/* Effets étoile du BG */}
+    <StarBackground />
       {/* Navbar*/}
 
       {/* contenu principal */}
