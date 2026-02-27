70890e88c5788c80a0bfdcb10aded45c3561334a
TheoHcht-smart <theo9925.huchot@gmail.com>
2026-02-27 18:13:07 +0100

    padding section done

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -7,8 +7,8 @@ import { technologies } from "../components/constants/index";
  const Skills = () => {
 
 return (
-    <section id="skills" className=" px-4 relative">
-    <h2 className=" text-3xl md:text-4xl font-bold mb-25 text-center">Compétences</h2>
+    <section id="skills" className=" py-25 px-4 relative">
+    <h2 className=" text-3xl md:text-4xl font-bold mb-25 text-center">Compétences <span className="text-primary">techniques</span> .</h2>
     <div className='flex flex-row flex-wrap justify-center gap-10'>
         
       {technologies.map((technology) => (
